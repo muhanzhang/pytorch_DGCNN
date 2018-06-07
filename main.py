7a1210abf85782cea96486c2e992514285904fab
YJQ@Nvidia1080Ti <littlepretty881203@gmail.com>
2018-06-07 11:06:01 -0500

    Fix runtime err; Raise issue of failed training.
    1. Use isinstance() instead of type(); type() fails to recognize
    tensor.cuda.TensorFloat when mode=gpu.
    2. Training loss is not a list, after copied back to cpu.
    3. Question to author: on our GPU server, training loss never decreases
    and accuracy is constantly 0. The learning seems not working.
    
    Signed-off-by: YJQ@Nvidia1080Ti <littlepretty881203@gmail.com>

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import math
 import pdb
-from DGCNN_embedding import DGCNN 
+from DGCNN_embedding import DGCNN
 from mlp_dropout import MLPClassifier
 
 sys.path.append('%s/pytorch_structure2vec-master/s2v_lib' % os.path.dirname(os.path.realpath(__file__)))
@@ -33,15 +33,15 @@ class Classifier(nn.Module):
             sys.exit()
 
         if cmd_args.gm == 'DGCNN':
-            self.s2v = model(latent_dim=cmd_args.latent_dim, 
+            self.s2v = model(latent_dim=cmd_args.latent_dim,
                             output_dim=cmd_args.out_dim,
-                            num_node_feats=cmd_args.feat_dim+cmd_args.attr_dim, 
+                            num_node_feats=cmd_args.feat_dim+cmd_args.attr_dim,
                             num_edge_feats=0,
                             k=cmd_args.sortpooling_k)
         else:
-            self.s2v = model(latent_dim=cmd_args.latent_dim, 
+            self.s2v = model(latent_dim=cmd_args.latent_dim,
                             output_dim=cmd_args.out_dim,
-                            num_node_feats=cmd_args.feat_dim, 
+                            num_node_feats=cmd_args.feat_dim,
                             num_edge_feats=0,
                             max_lv=cmd_args.max_lv)
         out_dim = cmd_args.out_dim
@@ -76,7 +76,7 @@ class Classifier(nn.Module):
             if node_feat_flag == True:
                 tmp = torch.from_numpy(batch_graph[i].node_features).type('torch.FloatTensor')
                 concat_feat.append(tmp)
-        
+
         if node_tag_flag == True:
             concat_tag = torch.LongTensor(concat_tag).view(-1, 1)
             node_tag = torch.zeros(n_nodes, cmd_args.feat_dim)
@@ -96,15 +96,15 @@ class Classifier(nn.Module):
             node_feat = torch.ones(n_nodes, 1)  # use all-one vector as node features
 
         if cmd_args.mode == 'gpu':
-            node_feat = node_feat.cuda() 
+            node_feat = node_feat.cuda()
             labels = labels.cuda()
 
         return node_feat, labels
 
-    def forward(self, batch_graph): 
+    def forward(self, batch_graph):
         node_feat, labels = self.PrepareFeatureLabel(batch_graph)
         embed = self.s2v(batch_graph, node_feat, None)
-        
+
         return self.mlp(embed, labels)
 
 def loop_dataset(g_list, classifier, sample_idxes, optimizer=None, bsize=cmd_args.batch_size):
@@ -118,13 +118,13 @@ def loop_dataset(g_list, classifier, sample_idxes, optimizer=None, bsize=cmd_arg
 
         batch_graph = [g_list[idx] for idx in selected_idx]
         _, loss, acc = classifier(batch_graph)
-        
+
         if optimizer is not None:
             optimizer.zero_grad()
-            loss.backward()         
+            loss.backward()
             optimizer.step()
 
-        loss = loss.data.cpu().numpy()[0]
+        loss = loss.data.cpu().numpy()
         pbar.set_description('loss: %0.5f acc: %0.5f' % (loss, acc) )
 
         total_loss.append( np.array([loss, acc]) * len(selected_idx))
@@ -153,7 +153,7 @@ if __name__ == '__main__':
     classifier = Classifier()
     if cmd_args.mode == 'gpu':
         classifier = classifier.cuda()
-    
+
     optimizer = optim.Adam(classifier.parameters(), lr=cmd_args.learning_rate)
 
     train_idxes = list(range(len(train_graphs)))
@@ -163,7 +163,7 @@ if __name__ == '__main__':
         classifier.train()
         avg_loss, _ = loop_dataset(train_graphs, classifier, train_idxes, optimizer=optimizer)
         print('\033[92maverage training of epoch %d: loss %.5f acc %.5f\033[0m' % (epoch, avg_loss[0], avg_loss[1]))
-        
+
         classifier.eval()
         test_loss, test_acc = loop_dataset(test_graphs, classifier, list(range(len(test_graphs))))
         print('\033[93maverage test of epoch %d: loss %.5f acc %.5f\033[0m' % (epoch, test_loss[0], test_loss[1]))
